4cf2767a153ccb3cf7a4f92314d40f6c57ae3a67
tanishaprasanna <103133165+tanishaprasanna@users.noreply.github.com>
2023-03-22 17:21:55 +0530

    Add files via upload

---
+++ Tic Tac Toe/script.js
@@ -0,0 +1,111 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+
+const winningPositions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+
+// initialise the game
+function initGame() {
+    currentPlayer = "O";
+    gameGrid = ["","","","","","","","",""];
+
+    boxes.forEach((box,index) => {
+        box.innerText = "";
+        boxes[index].style.pointerEvents = "all";
+        box.classList = `box box${index+1}`;
+
+        
+    });
+
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+initGame();
+
+function swapTurn() {
+    if(currentPlayer === "O"){
+        currentPlayer = "X";
+    }
+    else{
+        currentPlayer = "O";
+    }
+    gameInfo.innerText = `Current Player - ${currentPlayer}` ;
+}
+
+function handleClick(index) {
+    if(gameGrid[index] === "" ) {
+        boxes[index].innerText = currentPlayer;
+        gameGrid[index] = currentPlayer;
+        boxes[index].style.pointerEvents = "none";
+        swapTurn();
+        checkGameOver();
+    }
+}
+
+boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
+        handleClick(index);
+    })
+});
+
+newGameBtn.addEventListener("click", initGame);
+
+function checkGameOver() {
+    let answer = "";
+
+    winningPositions.forEach((position) => {
+        //all 3 boxes should be non-empty and exactly same in value
+        if( (gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "") 
+            && (gameGrid[position[0]] === gameGrid[position[1]] ) && (gameGrid[position[1]] === gameGrid[position[2]])) {
+
+                //check if winner is X
+                if(gameGrid[position[0]] === "X") 
+                    answer = "X";
+                else {
+                    answer = "O";
+                } 
+                    
+                boxes.forEach((box) => {
+                    box.style.pointerEvents = "none";
+                })
+
+                //show winner
+                boxes[position[0]].classList.add("win");
+                boxes[position[1]].classList.add("win");
+                boxes[position[2]].classList.add("win");
+            }
+    });
+
+    if(answer !== "" ) {
+        gameInfo.innerText = `Winner Player - ${answer}`;
+        newGameBtn.classList.add("active");
+        return;
+    }
+
+    let fillCount = 0;
+    gameGrid.forEach((box) => {
+        if(box !== "" )
+            fillCount++;
+    });
+
+    if(fillCount === 9) {
+        gameInfo.innerText = "Game Tied !";
+        newGameBtn.classList.add("active");
+    }
+
+}
+
